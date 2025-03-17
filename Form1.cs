9ba3709b563cc3cd8959a36115c24d4b891c9395
Marco <marcoamorim911@gmail.com>
2025-03-17 12:15:35 +0000

    pdf almost done

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using iTextSharp.text.pdf;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Diagnostics;
 using System.Windows.Forms.VisualStyles;
+using System.Drawing;
 
 namespace Gerador_ecxel
 {
@@ -59,6 +60,7 @@ namespace Gerador_ecxel
 
 		public async Task GeneratePdf(List<string[]> entries)
 		{
+			var baseColor = new BaseColor(75, 85, 87, 255);
 			string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"Relatorio_Notion_{_colaborador}.pdf");
 			Document doc = new Document(PageSize.A4);
 			PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
@@ -76,14 +78,15 @@ namespace Gerador_ecxel
 			BaseFont bfcalibriBIt = BaseFont.CreateFont(fontPath, BaseFont.WINANSI, BaseFont.EMBEDDED);
 
 			fontPath = Path.Combine(repoRoot, "Fontes\\verdana.ttf");
-			BaseFont bfVerdana = BaseFont.CreateFont(fontPath, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
+			BaseFont bfVerdana = BaseFont.CreateFont(fontPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
 
-			iTextSharp.text.Font titleFont = new iTextSharp.text.Font(bfcalibriBold, 15, iTextSharp.text.Font.BOLD);
-			iTextSharp.text.Font subTitleFont = new iTextSharp.text.Font(bfVerdana, 12, iTextSharp.text.Font.BOLD, new BaseColor(75, 86, 98, 255));
-			iTextSharp.text.Font tableHeaderFont = new iTextSharp.text.Font(bfcalibriBIt, 11, iTextSharp.text.Font.BOLD);
-			iTextSharp.text.Font tableFont = new iTextSharp.text.Font(bfCalibri, 10, iTextSharp.text.Font.NORMAL);
-			iTextSharp.text.Font sectionFont = new iTextSharp.text.Font(bfVerdana, 11, iTextSharp.text.Font.NORMAL);
-			iTextSharp.text.Font boldFont = new iTextSharp.text.Font(bfcalibriBold, 9, iTextSharp.text.Font.BOLD);
+			iTextSharp.text.Font titleFont = new iTextSharp.text.Font(bfcalibriBold, 13, iTextSharp.text.Font.BOLD);
+			iTextSharp.text.Font subTitleFont = new iTextSharp.text.Font(bfVerdana, 12, iTextSharp.text.Font.BOLD, baseColor);
+			iTextSharp.text.Font tableHeaderFont = new iTextSharp.text.Font(bfcalibriBIt, 11, iTextSharp.text.Font.BOLD, baseColor);
+			iTextSharp.text.Font tableFont = new iTextSharp.text.Font(bfCalibri, 8, iTextSharp.text.Font.NORMAL, baseColor);
+			iTextSharp.text.Font sectionFont = new iTextSharp.text.Font(bfVerdana, 10, iTextSharp.text.Font.NORMAL, baseColor);
+			iTextSharp.text.Font cellFont = new iTextSharp.text.Font(bfVerdana, 7, iTextSharp.text.Font.NORMAL, baseColor);
+			iTextSharp.text.Font boldFont = new iTextSharp.text.Font(bfcalibriBold, 9, iTextSharp.text.Font.BOLD, baseColor);
 			Paragraph title = new Paragraph("DESPESAS DE KM EM VIATURA PRÓPRIA\n", titleFont)
 			{
 				Alignment = Element.ALIGN_CENTER
@@ -92,78 +95,126 @@ namespace Gerador_ecxel
 			doc.Add(new Paragraph("\n"));
 
 			doc.Add(new Paragraph("Identificação: \n\n", subTitleFont));
+
 			PdfPTable identificacaoTable = new PdfPTable(2);
 			identificacaoTable.WidthPercentage = 100;
 			identificacaoTable.SetWidths(new float[] { 30f, 70f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Utilizador:", sectionFont, 14, "nenhuma");
+			AddCell(identificacaoTable, _colaborador, sectionFont, 14, "cheia");
+			doc.Add(identificacaoTable);
+			doc.Add(new Paragraph("\n"));
 
-			AddCell(identificacaoTable, "Utilizador:", sectionFont, 25f, "nenhuma");
-			AddCell(identificacaoTable, _colaborador, sectionFont, 25f, "cheia");
-			AddCell(identificacaoTable, "Nº Colaborador:", sectionFont, 25f, "nenhuma");
-			AddCell(identificacaoTable, "171", sectionFont, 25f, "cheia");
-			AddCell(identificacaoTable, "Empresa:", sectionFont, 25f, "nenhuma");
-			AddCell(identificacaoTable, "Expressglass SA", sectionFont, 25f, "cheia");
-			AddCell(identificacaoTable, "Centro de Custo:", sectionFont, 25f, "nenhuma");
-			AddCell(identificacaoTable, "BARCELOS", sectionFont, 25f, "cheia");
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 60;
+			identificacaoTable.SetWidths(new float[] { 30f, 30f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Nº Colaborador:", sectionFont, 14f, "nenhuma");
+			AddCellAlign(identificacaoTable, "171", sectionFont, 14f, "cheia", Element.ALIGN_CENTER);
+			doc.Add(identificacaoTable);
+			doc.Add(new Paragraph("\n"));
 
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 60;
+			identificacaoTable.SetWidths(new float[] { 30f, 30f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Empresa:", sectionFont, 14f, "nenhuma");
+			AddCellAlign(identificacaoTable, "Expressglass SA", sectionFont, 14f, "cheia", Element.ALIGN_CENTER);
+			doc.Add(identificacaoTable);
+			doc.Add(new Paragraph("\n"));
+
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 60;
+			identificacaoTable.SetWidths(new float[] { 30f, 30f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Centro de Custo:", sectionFont, 14f, "nenhuma");
+			AddCell(identificacaoTable, "BARCELOS", sectionFont, 14f, "cheia");
 			doc.Add(identificacaoTable);
 			doc.Add(new Paragraph("\n"));
 
 			doc.Add(new Paragraph("Despesas - Mapa de Km \n\n", subTitleFont));
-			PdfPTable DespesasTable = new PdfPTable(2);
-			DespesasTable.WidthPercentage = 100;
-			DespesasTable.SetWidths(new float[] { 30f, 70f });
-			AddCell(DespesasTable, "Data",sectionFont, 25f, "nenhuma");
-			AddCell(DespesasTable, _data, sectionFont, 25f, "Cheia");
-			AddCell(DespesasTable, "Matrícula:", sectionFont, 25f, "nenhuma");
-			AddCell(DespesasTable, _matricula, sectionFont, 25f, "cheia");
-			AddCell(DespesasTable, "Proprietário:", sectionFont, 25f, "nenhuma");
-			AddCell(DespesasTable, "IDFK", sectionFont, 25f, "cheia");
-			doc.Add(DespesasTable);
+
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 60;
+			identificacaoTable.SetWidths(new float[] { 30f, 30f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Data",sectionFont, 14f, "nenhuma");
+			AddCell(identificacaoTable, _data, sectionFont, 14f, "Cheia");
+			doc.Add(identificacaoTable);
+			doc.Add(new Paragraph("\n"));
+
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 60;
+			identificacaoTable.SetWidths(new float[] { 30f, 30 });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Matrícula:", sectionFont, 14f, "nenhuma");
+			AddCell(identificacaoTable, _matricula, sectionFont, 14f, "cheia");
+			doc.Add(identificacaoTable);
+			doc.Add(new Paragraph("\n"));
+
+			identificacaoTable = new PdfPTable(2);
+			identificacaoTable.WidthPercentage = 100;
+			identificacaoTable.SetWidths(new float[] { 30f, 70f });
+			identificacaoTable.HorizontalAlignment = Element.ALIGN_LEFT;
+			AddCell(identificacaoTable, "Proprietário:", sectionFont, 14f, "nenhuma");
+			AddCell(identificacaoTable, _colaborador, sectionFont, 14f, "cheia");
+			doc.Add(identificacaoTable);
 			doc.Add(new Paragraph("\n"));
 
 
 			PdfPTable table = new PdfPTable(6);
 			table.WidthPercentage = 100;
-			table.SetWidths(new float[] { 15f, 15f, 15f, 10f, 45f, 45f});
+			table.SetWidths(new float[] { 25f, 15f, 15f, 10f, 25f, 45f});
 
 			string[] headers = { "Dia", "Saída", "Chegada", "Km's", "Local", "Motivo" };
 			foreach (var header in headers)
 			{
 				PdfPCell cell = new PdfPCell(new Phrase(header, tableHeaderFont))
 				{
-					HorizontalAlignment = Element.ALIGN_CENTER,
-					Padding = 5f
+					HorizontalAlignment = Element.ALIGN_LEFT,
+					FixedHeight = 15f,
 				};
 				table.AddCell(cell);
 			}
-
 			foreach (var entry in entries)
 			{
 				string status = entry[6];
-				if (status != "Novo") continue;
+				if (status != "Novo" || entry[1] != _colaborador) continue;
 				string[] row = { entry[0], "09H00", "18H00", entry[5], entry[3], entry[4] };
 				foreach (var dataValue in row)
 				{
-					PdfPCell cell = new PdfPCell(new Phrase(dataValue, tableFont))
+					if (int.TryParse(dataValue, out int number) || DateTime.TryParse(dataValue, out DateTime date))
 					{
-						HorizontalAlignment = Element.ALIGN_CENTER,
-						Padding = 5f
-					};
-					table.AddCell(cell);
+						PdfPCell cellInt = new PdfPCell(new Phrase(dataValue, tableFont))
+						{
+							HorizontalAlignment = Element.ALIGN_RIGHT,
+							FixedHeight = 15f,
+						};
+						table.AddCell(cellInt);
+					}
+					else
+					{
+						PdfPCell cell = new PdfPCell(new Phrase(dataValue, tableFont))
+						{
+							HorizontalAlignment = Element.ALIGN_CENTER,
+							FixedHeight = 15f,
+						};
+						table.AddCell(cell);
+					}
 				}
 			}
 			doc.Add(table);
 			doc.Add(new Paragraph("\n"));
 
 			PdfPTable summaryTable = new PdfPTable(2);
-			summaryTable.WidthPercentage = 100;
-			summaryTable.SetWidths(new float[] { 50f, 50f });
-			AddCell(summaryTable, "Total Km:", sectionFont, 20f, "nenhuma");
-			AddCell(summaryTable, _klm.ToString(), tableFont, 20f, "nenhuma");
-			AddCell(summaryTable, "Valor/Km:", sectionFont, 20f, "nenhuma");
-			AddCell(summaryTable, "0,36 €", tableFont, 20f, "nenhuma");
-			AddCell(summaryTable, "Total de Despesas:", sectionFont, 20f, "cheia");
-			AddCell(summaryTable, "46,80 €", tableFont, 20f, "cheia");
+			summaryTable.SetWidths(new float[] { 20, 15 });
+			summaryTable.WidthPercentage = 20;
+			summaryTable.HorizontalAlignment = Element.ALIGN_CENTER;
+			AddCell(summaryTable, "Total Km", sectionFont, 20, "nenhuma");
+			AddCellUL(summaryTable, _klm.ToString(), sectionFont, 20, Element.ALIGN_RIGHT);
+			AddCell(summaryTable, "Valor/Km", sectionFont, 20, "nenhuma");
+			AddCellUL(summaryTable, "0,36 €", sectionFont, 20, Element.ALIGN_RIGHT);
+			AddCell(summaryTable, "Total de Despesas:" + "Conta", sectionFont, 20, "cheia");
 			doc.Add(summaryTable);
 			doc.Add(new Paragraph("\n"));
 
@@ -176,10 +227,10 @@ namespace Gerador_ecxel
 
 		private void AddCell(PdfPTable table, string text, iTextSharp.text.Font font, float height, string borderType)
 		{
+			var cellColor = new BaseColor(75, 86, 98, 255);
 			PdfPCell cell = new PdfPCell(new Phrase(text, font))
 			{
 				HorizontalAlignment = Element.ALIGN_LEFT,
-				Padding = 5f,
 				FixedHeight = height
 			};
 			if (borderType == "nenhuma")
@@ -189,10 +240,50 @@ namespace Gerador_ecxel
 			else if (borderType == "fina")
 			{
 				cell.BorderWidth = 0.5f;
+				cell.BorderColor = cellColor;
 			}
 			else if (borderType == "cheia")
 			{
-				cell.BorderWidth = 1.5f;
+				cell.BorderWidth = 1.2f;
+				cell.BorderColor = cellColor;
+			}
+			table.AddCell(cell);
+		}
+		private void AddCellUL(PdfPTable table, string text, iTextSharp.text.Font font, float height, int align)
+		{
+			var cellColor = new BaseColor(75, 86, 98, 255);
+			PdfPCell cell = new PdfPCell(new Phrase(text, font))
+			{
+				HorizontalAlignment = align,
+				FixedHeight = height,
+				Border = PdfPCell.BOTTOM_BORDER
+			};
+			table.AddCell(cell);
+		}
+		private void AddCellAlign(PdfPTable table, string text, iTextSharp.text.Font font, float height, string borderType, int align)
+		{
+			var cellColor = new BaseColor(75, 86, 98, 255);
+			iTextSharp.text.Font underlineFont = new iTextSharp.text.Font(font);
+			underlineFont.SetStyle(iTextSharp.text.Font.UNDERLINE);
+			PdfPCell cell = new PdfPCell(new Phrase(new Chunk(text, underlineFont)))
+			{
+				HorizontalAlignment = align,
+				FixedHeight = height
+			};
+			if (borderType == "nenhuma")
+			{
+				cell.BorderColor = cellColor;
+				cell.Border = PdfPCell.NO_BORDER;
+			}
+			else if (borderType == "fina")
+			{
+				cell.BorderWidth = 0.5f;
+				cell.BorderColor = cellColor;
+			}
+			else if (borderType == "cheia")
+			{
+				cell.BorderWidth = 1.2f;
+				cell.BorderColor = cellColor;
 			}
 			table.AddCell(cell);
 		}
